edb1fc43790314a02fea2dcba83d709b356ac9a8
THToufique <thtoufique@gmail.com>
2026-09-23 11:59:40 +0000

    begonia: re-enable LLVM_POLLY and add ION fb heap for Clang 21 build
    - begonia_user_defconfig: CONFIG_LLVM_POLLY=y (the disable commit was a
      Clang 22 workaround; kernel now builds with Clang 21)
    - mtkfb.c: ion_drv_create_FB_heap() (heap 11) so gralloc4 framebuffer
      allocations succeed

--- drivers/misc/mediatek/video/mt6785/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6785/videox/mtkfb.c
@@ -2804,15 +2804,11 @@ static int mtkfb_probe(struct platform_device *pdev)
 	primary_display_diagnose(__func__, __LINE__);
 #endif
 /*disp decouple will use this buffer*/
-#if 0
 	/*
 	 * this function will get fb_heap base address to ion
 	 * for management frame buffer
 	 */
-#ifdef MTK_FB_ION_SUPPORT
-	/*ion_drv_create_FB_heap(mtkfb_get_fb_base(), mtkfb_get_fb_size());*/
-#endif
-#endif
+	ion_drv_create_FB_heap(mtkfb_get_fb_base(), mtkfb_get_fb_size());
 	fbdev->state = MTKFB_ACTIVE;
 
 	MSG_FUNC_LEAVE();
